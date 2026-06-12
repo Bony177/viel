d31f5a465416dd8f125a92c9299861a7d7f92db1
Bony177 <bonybabu61@gmail.com>
2026-06-12 17:33:41 +0530

    In

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import HeroSection from "./components/HeroSection";
 import SecondSection from "./components/SecondSection";
+import fogVideo from "./assets/videos/fog.webm";
+import "./styles/FogBridge.css";
 
 function App() {
   return (
-    <>
+    <div style={{ position: "relative" }}>
       <HeroSection />
+      {/* Fog Transition Bridge Between Sections */}
+      <div className="fog-bridge">
+        <video className="fog-bridge-video" autoPlay muted loop playsInline>
+          <source src={fogVideo} type="video/webm" />
+        </video>
+      </div>
       <SecondSection />
-    </>
+    </div>
   );
 }
 
